ae06d47d0b44bd47d4b78ef9e2c994b4b4cd8b4c
Ziad Chamrah <147912275+Chamrah@users.noreply.github.com>
2023-11-20 14:13:27 +0100

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,27 @@
+// // This is a class
+public class Main{
+// Function that which contains an arguments
+    public static void main(String[] args) {
+//  First player
+        Player firstplayer = new Player();
+        firstplayer.name ="Cristiano Ronaldo";
+        firstplayer.age = 38;
+        firstplayer.rank = 1;
+        System.out.println(firstplayer.name+" have "+firstplayer.age+" his rank is "+firstplayer.rank);
+//        firstplayer.pass();
+//        firstplayer.jump();
+        firstplayer.shoot();
+
+// Second player
+        Player secondplayer = new Player();
+        secondplayer.name ="Lionnel Messi";
+        secondplayer.age = 36;
+        secondplayer.rank = 2;
+        System.out.println(secondplayer.name+" have "+secondplayer.age+" his rank is "+secondplayer.rank);
+        secondplayer.pass();
+//        secondplayer.jump();
+//        secondplayer.shoot();
+
+
+    }
+}
---
+++ Player.java
@@ -0,0 +1,17 @@
+// Class that define the objects (UML)
+public class Player {
+    String name;
+    short age;
+    short rank;
+
+// Method that do the pass and the shoot and the jump of player
+    void pass(){
+        System.out.print(name + " Passing");
+    }
+    void shoot(){
+        System.out.print(name + " Shooting");
+    }
+    void jump(){
+        System.out.print(name + " Jumping");
+    }
+}
